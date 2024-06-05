c7299c9fd1396373e30a02dfd62fc0378d6a8193
Ragnar Östh <ragnar.osth@afry.com>
2024-06-05 16:28:08 +0200

    SignIn SignUp Layouts added

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -1,33 +1,33 @@
-import {
-  ClerkProvider,
-  SignInButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from "@clerk/nextjs";
+import { ClerkProvider } from '@clerk/nextjs'
+import { dark } from '@clerk/themes'
 
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import Header from "../components/Header/Header";
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: "Snipia",
-  description: "The one place for all your code snippets",
-};
+  title: 'Snipia',
+  description: 'The one place for all your code snippets',
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+        variables: { colorPrimary: '#d44700' },
+        elements: { card: 'bg-main-700 shadow-none' },
+      }}
+    >
       <html lang="en">
         <body className={inter.className}>{children}</body>
       </html>
     </ClerkProvider>
-  );
+  )
 }
