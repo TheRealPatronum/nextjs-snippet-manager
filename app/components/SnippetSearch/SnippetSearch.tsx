f7773c71559563ce0765f76461c62a4b3d16be23
Ragnar Östh <ragnar.osth@afry.com>
2024-06-14 10:24:55 +0200

    added filtering for snippet search

--- app/components/SnippetSearch/SnippetSearch.tsx
+++ app/components/SnippetSearch/SnippetSearch.tsx
@@ -3,17 +3,32 @@
 import { Snippet } from '@prisma/client'
 import SearchBar from '../SearchBar/SearchBar'
 import SnippetList from '../SnippetList/SnippetList'
+import { useState } from 'react'
 
 export default function SnippetSearch(p: {
   placeholder: string
   snippets: Snippet[]
 }) {
+  const [curentSearchQuery, setCurentSearchQuery] = useState('')
+  const filteredSnippets = p.snippets.filter((snippet) => {
+    return [
+      snippet.title,
+      snippet.technology,
+      snippet.language,
+      snippet.content,
+    ].some((snippetAttribute) =>
+      snippetAttribute
+        .toLocaleLowerCase()
+        .includes(curentSearchQuery.toLocaleLowerCase()),
+    )
+  })
+
   return (
     <div>
-      <SearchBar placeholder={p.placeholder} onChange={(t) => console.log(t)} />
+      <SearchBar placeholder={p.placeholder} onChange={setCurentSearchQuery} />
       {/** a list of snippets */}
       <div className="overflow-y-auto h-[80vh] pb-20">
-        <SnippetList snippets={p.snippets} />
+        <SnippetList snippets={filteredSnippets} />
       </div>
     </div>
   )
