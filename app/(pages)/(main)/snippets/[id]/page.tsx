5b21e285dab053dca9b1c74bbbbe4cb5db45eed1
Ragnar Östh <ragnar.osth@afry.com>
2024-06-14 11:02:10 +0200

    added syntax highlighter

--- app/(pages)/(main)/snippets/[id]/page.tsx
+++ app/(pages)/(main)/snippets/[id]/page.tsx
@@ -1,9 +1,10 @@
 import { readSnippet } from '@/app/api/snippet/[id]/service'
+import { SnippetDetail } from '@/app/components/SnippetDetail/SnippetDetail'
 
 export default async function SnippetDetailPage(p: { params: { id: string } }) {
   const { data: snippet } = await readSnippet(Number(p.params.id))
   if (snippet === null) {
     return <div>Snippet not found...</div>
   }
-  return <>{JSON.stringify(snippet)}</>
+  return <SnippetDetail snippet={snippet} />
 }
---
+++ app/components/SnippetDetail/SnippetDetail.tsx
@@ -0,0 +1,35 @@
+import { SNIPPETS_METADATA } from '@/app/constant'
+import { Snippet } from '@prisma/client'
+import Image from 'next/image'
+import { Prism as SynthaxHighlighter } from 'react-syntax-highlighter'
+import { atomDark as theme } from 'react-syntax-highlighter/dist/esm/styles/prism'
+
+export function SnippetDetail(p: { snippet: Snippet }) {
+  const snippetMetadata = SNIPPETS_METADATA[p.snippet.technology]
+
+  const codeHighLighter = (
+    <SynthaxHighlighter
+      showLineNumbers
+      style={theme}
+      language={p.snippet.language}
+    >
+      {p.snippet.content}
+    </SynthaxHighlighter>
+  )
+  const title = (
+    <div className="flex space-x-4">
+      <Image
+        className="w-10"
+        src={snippetMetadata.src}
+        alt="Programming language image"
+      />
+      <h1>{p.snippet.title}</h1>
+    </div>
+  )
+  return (
+    <div>
+      {title}
+      <div className="mt-10">{codeHighLighter}</div>
+    </div>
+  )
+}
