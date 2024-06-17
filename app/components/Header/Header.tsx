bdbc798ca1f55ed7284c7b5625258976f48c89cb
Ragnar Östh <ragnar.osth@afry.com>
2024-06-17 11:22:50 +0200

    added Language switcher

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -1,15 +1,38 @@
+'use client'
+import { TbWorld } from 'react-icons/tb'
 import Link from 'next/link'
 import Image from 'next/image'
 import logoPng from '/public/logo.png'
 import { UserButton } from '@clerk/nextjs'
+import { useLocale } from 'next-intl'
+import { usePathname, useRouter } from 'next/navigation'
 
 export default function Header(p: {}) {
+  const locale = useLocale()
+  const router = useRouter()
+  const pathname = usePathname()
+
+  // TODO ADD FLAGSELECT DROPDOWN
+  const toggleLanguage = () => {
+    router.replace(
+      (locale === 'en' ? '/fr' : '/en') + pathname.split('/' + locale)[1],
+    )
+  }
+
   return (
     <header className="bg-main-700 p-8 flex justify-between items-center">
       <Link href="/">
         <Image src={logoPng} alt="Logo" className="w-48" />
       </Link>
-      <UserButton />
+      <div className="flex items-center justify-center space-x-12">
+        <TbWorld
+          className="text-white hover:text-white/80 cursor-pointer"
+          onClick={toggleLanguage}
+        />
+        <div className="w-12">
+          <UserButton />
+        </div>
+      </div>
     </header>
   )
 }
