e48897e6d6c767c66d8763674cfab1fac88ed1af
Ragnar Östh <ragnar.osth@afry.com>
2024-06-17 10:33:10 +0200

    translation for clerk components

--- app/(pages)/[locale]/layout.tsx
+++ app/(pages)/[locale]/layout.tsx
@@ -5,6 +5,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { Toaster } from 'sonner'
+import { frFR, svSE, enUS } from '@clerk/localizations'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,23 +14,30 @@ export const metadata: Metadata = {
   description: 'The one place for all your code snippets',
 }
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode
-}>) {
+const clerkI18n = {
+  fr: frFR,
+  en: enUS,
+  se: svSE,
+}
+export default function RootLayout(
+  p: Readonly<{
+    children: React.ReactNode
+    params: { locale: 'en' | 'fr' | 'se' }
+  }>,
+) {
   return (
     <ClerkProvider
+      localization={clerkI18n[p.params.locale]}
       appearance={{
         baseTheme: dark,
         variables: { colorPrimary: '#d44700' },
         elements: { card: 'bg-main-700 shadow-none' },
       }}
     >
-      <html lang="en">
+      <html lang={p.params.locale}>
         <body className={inter.className}>
           <Toaster richColors />
-          {children}
+          {p.children}
         </body>
       </html>
     </ClerkProvider>
